cda1774b970c5685835f660c5621c93b811dc31e
MiguelGuedes1 <miguelsilvaguedes1@gmail.com>
2025-01-19 21:51:50 +0000

    Rotas e tailwind defenido

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 import './App.css'
+import {Route,Routes} from "react-router-dom"
+import Home from './pages/Home'
 
 function App() {
  
 
   return (
-    <div>
-      <h1>Pagina Home</h1>
-    </div>
+    <Routes>
+      <Route path='/' element={<Home/>} />
+    </Routes>
   )
 }
 
